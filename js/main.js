4cfbf05d694dbbc5b115b05010ae60cf03490fea
gagarine <simon@antistatique.net>
2013-10-12 21:56:02 +0200

    finnaly pass the button context correctly

--- js/main.js
+++ js/main.js
@@ -10,42 +10,55 @@
  * @constructor
  */
 function SimonGame(btns, synthesizer) {
-  this.btns = btns;
-  this.synthesizer = synthesizer;
-  this.numberOfLevel = 50;
-  this.currentLevel = 0;
-  this.sequence = '';
-  this.playingStep = null;
-  this.speed = 300;
+    this.btns = btns;
+    this.synthesizer = synthesizer;
+    this.numberOfLevel = 50;
+    this.currentLevel = 0;
+    this.sequence = '';
+    this.playingStep = null;
+    this.speed = 350;
 }
 
 SimonGame.prototype.play = function () {
-  // this.btnGreen.lightUp();
-  // this.synthesizer.play(this.btnGreen.soundFrequency);
-  this.sequence = this.generateSequence();
-  this.playSequence(0);
+    // this.btnGreen.lightUp();
+    // this.synthesizer.play(this.btnGreen.soundFrequency);
+    this.sequence = this.generateSequence();
+    this.playSequence(0);
+
+    var that = this;
+    for (var key in this.btns) {
+        (function (currentBtn, synthetiser) {
+            currentBtn.element.addEventListener( 'mousedown', function () {
+                currentBtn.lightUp();
+                synthetiser.play(currentBtn.soundFrequency);
+            }, false);
+            currentBtn.element.addEventListener('mouseup', function () {
+                currentBtn.lightDown();
+                synthetiser.stop();
+            }, false);
+        })(this.btns[key],this.synthesizer);
+    }
 };
 
 SimonGame.prototype.playSequence = function (step) {
-  var that = this; // stupid JS
-  this.btns[this.sequence[step]].lightUp();
-  this.synthesizer.play(this.btns[this.sequence[step]].soundFrequency);
-  setTimeout(function() {
-    console.log(step);
-    that.btns[that.sequence[step]].lightDown();
-    //that.synthesizer.stop(); //
-    if (step < that.numberOfLevel) {
-        that.playSequence(step + 1);
-      }
+    var that = this; // stupid JS
+    this.btns[this.sequence[step]].lightUp();
+    this.synthesizer.play(this.btns[this.sequence[step]].soundFrequency);
+    setTimeout(function () {
+        that.btns[that.sequence[step]].lightDown();
+        that.synthesizer.stop(); //
+        if (step < that.currentLevel) {
+            that.playSequence(step + 1);
+        }
     }, this.speed);
 }
 
 SimonGame.prototype.generateSequence = function () {
-  var sequence = "";
-  for (var i = 0; i < this.numberOfLevel; i++) {
-    sequence += Math.floor((Math.random() * 4)); // add a random number from 0 to 3 (we can us btns.lenght...)
-  }
-  return sequence;
+    var sequence = "";
+    for (var i = 0; i < this.numberOfLevel; i++) {
+        sequence += Math.floor((Math.random() * 4)); // add a random number from 0 to 3 (we can use btns.lenght...)
+    }
+    return sequence;
 }
 
 
@@ -57,25 +70,24 @@ SimonGame.prototype.generateSequence = function () {
  * @constructor
  */
 function Button(element, soundFrequency) {
-  this.element = element;
-  this.soundFrequency = soundFrequency;
+    this.element = element;
+    this.soundFrequency = soundFrequency;
 }
 
 Button.prototype.lightUp = function () {
-  this.element.classList.add("lightUp");
+    this.element.classList.add("lightUp");
 };
 
 Button.prototype.lightDown = function () {
-  this.element.classList.remove("lightUp");
+    this.element.classList.remove("lightUp");
 };
 
 Button.prototype.pressed = function () {
-  this.lightUp();
-
+    this.lightUp();
 };
 
 Button.prototype.release = function () {
-  this.lightDown();
+    this.lightDown();
 };
 
 /**
@@ -84,41 +96,41 @@ Button.prototype.release = function () {
  * @constructor
  */
 function Synthesizer() {
-  try {
-    this.audio_context = new (window.AudioContext || window.webkitAudioContext);
-    this.oscillator = this.audio_context.createOscillator();
-  } catch (e) {
-    alert('No web audio oscillator support in this browser');
-  }
+    try {
+        this.audio_context = new (window.AudioContext || window.webkitAudioContext);
+    } catch (e) {
+        alert('No web audio oscillator support in this browser');
+    }
 }
 
 Synthesizer.prototype.play = function (freq) {
-  this.oscillator.frequency.value = freq;
-  this.oscillator.connect(this.audio_context.destination);
-  this.oscillator.noteOn(0);
+    this.oscillator = this.audio_context.createOscillator();
+    this.oscillator.frequency.value = freq;
+    this.oscillator.connect(this.audio_context.destination);
+    this.oscillator.start(0);
 };
 
 
 Synthesizer.prototype.stop = function () {
-  this.oscillator.noteOff(0);
+    this.oscillator.stop(0);
 };
 
 
 /////// INIT
 function init() {
-  var btns, synthesizer, mySimonGame;
+    var btns, synthesizer, mySimonGame;
 
-  btns = [];
+    btns = [];
 
-  btns.push(new Button(document.getElementById('btnGreen'), 440));
-  btns.push(new Button(document.getElementById('btnRed'), 400));
-  btns.push(new Button(document.getElementById('btnYellow'), 500));
-  btns.push(new Button(document.getElementById('btnBlue'), 200));
+    btns.push(new Button(document.getElementById('btnGreen'), 330));
+    btns.push(new Button(document.getElementById('btnRed'), 261));
+    btns.push(new Button(document.getElementById('btnYellow'), 392));
+    btns.push(new Button(document.getElementById('btnBlue'), 523));
 
-  synthesizer = new Synthesizer();
+    synthesizer = new Synthesizer();
 
-  mySimonGame = new SimonGame(btns, synthesizer);
-  mySimonGame.play();
+    mySimonGame = new SimonGame(btns, synthesizer);
+    mySimonGame.play();
 }
 
 
